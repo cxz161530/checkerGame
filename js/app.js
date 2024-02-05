cb419dcd7bce4e9969812dba8206d275b74f676f
cxz161530 <czzhu940222@gmail.com>
2024-02-05 15:23:06 -0800

    initiate piece location

--- js/app.js
+++ js/app.js
@@ -8,9 +8,18 @@ class GameBoardSquare{
     }
 }
 
+class Pieces{
+    constructor(x,y,color, alive){
+        this.x=x;
+        this.y=y;
+        this.color = color;
+        this.alive = alive;
+    } 
+    }
 
 //functions
-//function used to create gameBoard, squares array to hold all squares info
+//GameBoard function used to create gameBoard, squares array to hold all squares info
+//it will 1: create board in browser,2 make an array storing each square info 
 function createGameBoard(row, columns){
     let gameBoard = document.getElementById("gameBoard");
     let squares = [];
@@ -23,6 +32,8 @@ function createGameBoard(row, columns){
         //create div for each obj created
         let squareElement = document.createElement("div")
         squareElement.classList.add("square");
+        //set up a uniq id for each square
+        squareElement.setAttribute('id', (x*8 + y));
         //set up color
         squareElement.style.backgroundColor = square.color;
         gameBoard.appendChild(squareElement)
@@ -32,6 +43,20 @@ function createGameBoard(row, columns){
     console.log(squares);
     
 }
+//piece function create pieces on gameboard in black and right, keep track if the piece is alive or not
+function createPiece(row, column){
+    let redPiece = document.getElementById(""); 
+    const redStartId = [1,3,5,7,9,11,13,15,17,19,21,23] 
+    const blackStartId = [63,61,59,57,55,53,51,49,47,45,43,41]
+//need set up bacl and red pieces into those location to initiate , using id to link
+    
+}
 
+
+
+
+// Excuting functions
+//1. we set up a 8*8 gameboard.
 createGameBoard(8,8);
+//2. we set up pieces red and brown
 
