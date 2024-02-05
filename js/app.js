cc0d0bbd2d959b3d8d41b76cae9cf3fd30280571
cxz161530 <czzhu940222@gmail.com>
2024-02-05 11:25:20 -0800

    GameBoard can show on screen, finished css

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 //classes
 
-class gameBoardSquare{
+class GameBoardSquare{
     constructor(x,y,color){
         this.x=x;
         this.y = y;
@@ -18,18 +18,19 @@ function createGameBoard(row, columns){
         for(let y = 0; y<columns; y++){
         //color decided by x,y sum value, even is black, odd is white
         let color = (((x+y)%2)===0) ? 'black' :'white';
-        let square = new gameBoardSquare(x, y, color);
+        let square = new GameBoardSquare(x, y, color);
         squares.push(square);
         //create div for each obj created
         let squareElement = document.createElement("div")
         squareElement.classList.add("square");
         //set up color
-        squareElement.style.backgroundColor = color;
+        squareElement.style.backgroundColor = square.color;
         gameBoard.appendChild(squareElement)
         }
 
     }
     console.log(squares);
+    
 }
 
 createGameBoard(8,8);
