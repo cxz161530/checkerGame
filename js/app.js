5c520f0acc3cfcf9bb5d59799977b09953e1cac1
cxz161530 <czzhu940222@gmail.com>
2024-02-05 10:42:50 -0800

    Created gameBoard obj in array

--- js/app.js
+++ js/app.js
@@ -1,1 +1,36 @@
-console.log('connect')
+//classes
+
+class gameBoardSquare{
+    constructor(x,y,color){
+        this.x=x;
+        this.y = y;
+        this.color = color
+    }
+}
+
+
+//functions
+//function used to create gameBoard, squares array to hold all squares info
+function createGameBoard(row, columns){
+    let gameBoard = document.getElementById("gameBoard");
+    let squares = [];
+    for(let x = 0; x<row; x++){
+        for(let y = 0; y<columns; y++){
+        //color decided by x,y sum value, even is black, odd is white
+        let color = (((x+y)%2)===0) ? 'black' :'white';
+        let square = new gameBoardSquare(x, y, color);
+        squares.push(square);
+        //create div for each obj created
+        let squareElement = document.createElement("div")
+        squareElement.classList.add("square");
+        //set up color
+        squareElement.style.backgroundColor = color;
+        gameBoard.appendChild(squareElement)
+        }
+
+    }
+    console.log(squares);
+}
+
+createGameBoard(8,8);
+
