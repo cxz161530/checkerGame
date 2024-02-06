4a1289d151aa2bee49f38a69bcc90b246eaca308
cxz161530 <czzhu940222@gmail.com>
2024-02-06 10:53:10 -0800

    added pieces function

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+//constants
+const pieceImg = {
+    redPiece: 'img/brown piece.jpeg',
+    blackPiece:'img/red piece.jpeg'
+}
+
+
 //classes
 
 class GameBoardSquare{
@@ -18,6 +25,16 @@ class Pieces{
     }
 
 //functions
+// pieceInitial function used to initial all pieces in initial location
+function pieceInitial(squareId){
+    if(squareId<24 && squareId%2!==0){
+        return pieceImg.redPiece
+    }else if(squareId>40 &&squareId%2 !==0){
+        return pieceImg.blackPiece
+    }
+    return "";
+}
+
 //GameBoard function used to create gameBoard, squares array to hold all squares info
 //it will 1: create board in browser,2 make an array storing each square info 
 function createGameBoard(row, columns){
@@ -26,31 +43,33 @@ function createGameBoard(row, columns){
     for(let x = 0; x<row; x++){
         for(let y = 0; y<columns; y++){
         //color decided by x,y sum value, even is black, odd is white
-        let color = (((x+y)%2)===0) ? 'black' :'white';
+        let color = (((x+y)%2)===0) ? 'white' :'black';
         let square = new GameBoardSquare(x, y, color);
         squares.push(square);
         //create div for each obj created
         let squareElement = document.createElement("div")
         squareElement.classList.add("square");
         //set up a uniq id for each square
-        squareElement.setAttribute('id', (x*8 + y));
+        let squareId = x*8+y;
+        squareElement.setAttribute('id', squareId);
         //set up color
         squareElement.style.backgroundColor = square.color;
+        console.log(pieceInitial(squareId))
+        //put Piece into it
+        squareElement.style.backgroundImage = pieceInitial(squareId);
+        
+
+
+
         gameBoard.appendChild(squareElement)
         }
 
     }
     console.log(squares);
+
     
 }
-//piece function create pieces on gameboard in black and right, keep track if the piece is alive or not
-function createPiece(row, column){
-    let redPiece = document.getElementById(""); 
-    const redStartId = [1,3,5,7,9,11,13,15,17,19,21,23] 
-    const blackStartId = [63,61,59,57,55,53,51,49,47,45,43,41]
-//need set up bacl and red pieces into those location to initiate , using id to link
-    
-}
+
 
 
 
